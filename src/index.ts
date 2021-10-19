e5acc08a2fc6ecb53dfb68c0c185a66ad80acd4a
Administrator <Administrator@DESKTOP-OKRG5S0>
2021-10-19 16:58:44 +0800

    取消入口处设置版本号

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import { createApp } from "vue";
 import ElementPlus from 'element-plus';
 import App from "./App.vue";
-import setVersion from "./version";
 
 const app = createApp(App);
 app.use(ElementPlus).mount('#app');
-
-setVersion();
