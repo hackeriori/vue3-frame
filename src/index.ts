ce6fa64c12d6de2a833d3c45dd178b3cf829b950
Administrator <Administrator@DESKTOP-OKRG5S0>
2021-10-28 16:25:06 +0800

    添加本地化语言

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import ElementPlus from 'element-plus';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import App from "./App.vue";
 
-const app = createApp(App);
-app.use(ElementPlus).mount('#app');
+createApp(App).use(ElementPlus,{locale:zhCn}).mount('#app');
